112bcfe3564bb7294d615bf23bb373614f5e6bb3
tnmush <tnmush@yahoo.com>
2023-09-01 08:12:39 +0200

    Adding the CriticScore

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+interface Props {
+  criticScore: number;
+}
+
+const CriticScore = ({ criticScore }: Props) => {
+  let color = criticScore > 75 ? "green" : criticScore ? "yellow" : " ";
+  return (
+    <>
+      <Badge padding={1} borderRadius={"5px"}>
+        {criticScore}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, Heading, Image, CardBody, Text } from "@chakra-ui/react";
+import { Card, Heading, Image, CardBody, Text, HStack } from "@chakra-ui/react";
 import { Game } from "../Hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 interface Props {
   game: Game;
 }
@@ -10,9 +11,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore criticScore={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
